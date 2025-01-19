294d89016d87b72da5006cecdf0aeee9abd9eb2c
rituraj0506 <riturajkumar7256@gmail.com>
2025-01-19 22:49:44 +0530

    changes_added_splash_screen

--- finance_tracker/lib/main.dart
+++ finance_tracker/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:finance_tracker/Pages/HomePage.dart';
+import 'package:finance_tracker/SplashScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Homepage(),
+      home: SplashScreen(),
     );
   }
 }
